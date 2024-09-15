190da04fada9a12b43471dcdcaa1cdda2a65dc9a
Alexander Todorov <e-alexander.todorov@smart.com>
2024-09-15 20:23:27 +0300

    Adding links to services

--- deploy_local.sh
+++ deploy_local.sh
@@ -7,5 +7,5 @@ cp -R img/ dist/img
 echo "Copying assets folder to dist"
 yarn copy-assets
 echo "Copying views folder to dist"
-pm2 restart server.js
+pm2 restart server
 echo "Deployed successfully"
--- server.js
+++ server.js
@@ -4,32 +4,27 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist'), { fallthrough: true }));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Define specific routes for each HTML file
 app.get(['/calculator', '/calculator.html'], (req, res) => {
-    console.log('Serving calculator.html');
     res.sendFile(path.resolve(__dirname, 'dist', 'calculator.html'));
 });
 
 app.get(['/analysis', '/analysis.html'], (req, res) => {
-    console.log('Serving analysis.html');
     res.sendFile(path.resolve(__dirname, 'dist', 'analysis.html'));
 });
 
 app.get(['/results', '/results.html'], (req, res) => {
-    console.log('Serving results.html');
     res.sendFile(path.resolve(__dirname, 'dist', 'results.html'));
 });
 
 app.get(['/search', '/search.html'], (req, res) => {
-    console.log('Serving search.html');
     res.sendFile(path.resolve(__dirname, 'dist', 'search.html'));
 });
 
-// Catch-all route to serve index.html for other paths
+// Catch-all route to serve index.html for any undefined routes
 app.get('*', (req, res) => {
-    console.log(`Fallback: Serving index.html for path ${req.path}`);
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
