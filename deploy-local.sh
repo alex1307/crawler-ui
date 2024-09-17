ed7a8624cd2c1eeaca8a96fb34a6ff343d1b794c
Alexander Todorov <e-alexander.todorov@smart.com>
2024-09-17 10:22:44 +0300

    Add localizations

--- deploy-local.sh
+++ deploy-local.sh
@@ -5,6 +5,7 @@ echo "Building project"
 yarn build --emptyOutDir
 echo "Copying img folder to dist"
 cp -R img/ dist/img 
+cp -R locales/ dist/locales 
 echo "Copying assets folder to dist"
 yarn copy-assets
 echo "Copying views folder to dist"
